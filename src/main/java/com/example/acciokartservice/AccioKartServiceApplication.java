be0560d9b4036e64d1d006819ca81a88be5a4cc2
Owais Iqbal <owaisiqbal2013@gmail.com>
2024-03-10 16:57:04 +0530

    probably final

--- src/main/java/com/example/acciokartservice/AccioKartServiceApplication.java
+++ src/main/java/com/example/acciokartservice/AccioKartServiceApplication.java
@@ -3,10 +3,16 @@ package com.example.acciokartservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 public class AccioKartServiceApplication {
 
 	public static void main(String[] args) {
+
+//		List<Integer> nums = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
 		SpringApplication.run(AccioKartServiceApplication.class, args);
 	}
 
